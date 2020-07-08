0e8cd00aafa6c416cac6e1823499bd05a46ef77f
Tom Salvo <tom.salvo@gmail.com>
2020-07-08 08:42:43 -0700

    add support for Mapper 66 (GxROM), which has the same banking behavior as the already-implemented ColorDreams mapper

--- nes-emu-ios/nes-emu-ios/Model/NES/Mappers/Mapper_ColorDreams_GxROM.swift
+++ nes-emu-ios/nes-emu-ios/Model/NES/Mappers/Mapper_ColorDreams_GxROM.swift
@@ -1,15 +1,32 @@
 //
-//  Mapper_ColorDreams.swift
+//  Mapper_ColorDreams_GxROM.swift
 //  nes-emu-ios
 //
 //  Created by Tom Salvo on 7/8/20.
-//  Copyright © 2020 Tom Salvo. All rights reserved.
+//  Copyright © 2020 Tom Salvo.
 //
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
 
 import Foundation
 import os
 
-struct Mapper_ColorDreams: MapperProtocol
+struct Mapper_ColorDreams_GxROM: MapperProtocol
 {
     let hasStep: Bool = false
     
@@ -53,7 +70,7 @@ struct Mapper_ColorDreams: MapperProtocol
         case 0x6000 ... 0x7FFF:
             return self.sram[Int(aAddress - 0x6000)]
         default:
-            os_log("unhandled Mapper_ColorDreams read at address: 0x%04X", aAddress)
+            os_log("unhandled Mapper_ColorDreams_GxROM read at address: 0x%04X", aAddress)
             return 0
         }
     }
@@ -77,7 +94,7 @@ struct Mapper_ColorDreams: MapperProtocol
         case 0x6000 ... 0x7FFF:
             return self.sram[Int(aAddress - 0x6000)] = aValue
         default:
-            os_log("unhandled Mapper_ColorDreams write at address: 0x%04X", aAddress)
+            os_log("unhandled Mapper_ColorDreams_GxROM write at address: 0x%04X", aAddress)
             break
         }
     }
