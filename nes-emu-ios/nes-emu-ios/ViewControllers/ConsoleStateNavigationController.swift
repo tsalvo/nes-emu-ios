2de5f4c23e3cff17460eedbd5dce68e2c0ac9c45
Tom Salvo <tom.salvo@gmail.com>
2020-10-16 20:41:21 -0700

    add MIT license text to recently added source files

--- nes-emu-ios/nes-emu-ios/ViewControllers/ConsoleStateNavigationController.swift
+++ nes-emu-ios/nes-emu-ios/ViewControllers/ConsoleStateNavigationController.swift
@@ -5,6 +5,23 @@
 //  Created by Tom Salvo on 10/15/20.
 //  Copyright © 2020 Tom Salvo. All rights reserved.
 //
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
 
 import UIKit
 
@@ -22,16 +39,4 @@ class ConsoleStateNavigationController: UINavigationController
             safeConsoleStateVC.consoleSaveStateSelectionDelegate = self.consoleSaveStateSelectionDelegate
         }
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
